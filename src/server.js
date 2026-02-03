5b34d6bd9e0428d3be91eba1bf8bd143e0cebb1a
Jose Mateus <josemateus957@gmail.com>
2026-02-03 11:45:44 -0300

    feat: add structured message

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
  const http = require('node:http');
 
-http.createServer(function () {
-   console.log('The server was called.');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'content-type': 'text/html; charset=utf-8'});
+   response.end(JSON.stringify({message: 'Hello Client!'}));
 }).listen(3000);
